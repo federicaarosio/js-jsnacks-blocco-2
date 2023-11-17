1ed22298b97f38d7415f56ce82f6c8dedc9c050b
federicaarosio <arosio.fede@gmail.com>
2023-11-17 14:33:29 +0100

    snack-2 fixed

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,26 +1,39 @@
 // Generatore di "nomi cognomi" casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da questi due array distinti vuole generare una falsa lista di invitati con nome e cognome.
 
-//Ho lista 1
-//ho lista 2
 
-const nomi = [
+const firstNames = [
     "Marta",
     "Claudia",
     "Lorenzo",
     "Francesco"
 ];
 
-const cognomi = [
+const lastNames = [
     "Bianchi",
     "Rossi",
     "Martini",
     "Motta"
 ];
 
-//genenrare lista con nome e cognome
-    
-for (i = 0; i < nomi.length; i++) {
-    
-    const falseList = console.log(nomi[i] + " " + cognomi[i]);
 
+const numberOfRandomNames = 15;
+const resultingArray = [];
+
+
+while ( resultingArray.length < numberOfRandomNames ){
+    // un elemento random dal primo array
+    let firstNameGen = firstNames[ Math.floor( Math.random() * firstNames.length)] ;
+
+    //  un elemento random del secondo array
+    let lastNameGen = lastNames[ Math.floor( Math.random() * lastNames.length)];
+
+    // genero il nome-cognome completo
+    let completeNameGen = firstNameGen + ' ' + lastNameGen;
+
+    //  inseriemo completeNameGen in questo array
+    resultingArray.push(completeNameGen);
+
+    console.log(firstNameGen, lastNameGen);
 }
+
+console.log(resultingArray);
