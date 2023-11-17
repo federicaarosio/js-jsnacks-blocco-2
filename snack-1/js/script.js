abefce7d7c89be1c6bd1297ae6ef12dfd6d8d33a
federicaarosio <arosio.fede@gmail.com>
2023-11-17 11:33:58 +0100

    snack-1

---
+++ snack-1/js/script.js
@@ -0,0 +1,15 @@
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+const array = [];
+
+const userNumber = parseInt(prompt("Type a number"));
+
+let sum = 0;
+let index = 0
+
+while (sum < 50) {
+    
+    array.push(userNumber);
+    //quando termino?
+    i++
+}
