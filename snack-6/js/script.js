5e81448f66ea6ae98c6e537161a836a8c1501996
federicaarosio <arosio.fede@gmail.com>
2023-11-21 12:15:09 +0100

    working on snack-6

---
+++ snack-6/js/script.js
@@ -0,0 +1,13 @@
+// Chiedo all'utente un numero e genero attraverso una funzione un numero di quadrati equivalente al numero inserito dall'utente nel DOM, in qualsiasi forma. 
+
+const userNumber = parseInt(prompt("Type a number"));
+
+const wrapperElement = document.querySelector("section.wrapper");
+wrapperElement.append = ;
+
+function generateNSquares(index) {
+    for (let index = 0; index < array.length; index++) {        
+        const newDiv =  document.createElement("div");
+        newDiv.classList.add("square");
+    }
+}
