8b74877b2272d37d9103b9edbce52d71234b2520
federicaarosio <arosio.fede@gmail.com>
2023-11-23 12:28:46 +0100

    cronometro

---
+++ snack-cronometro/js/script.js
@@ -0,0 +1,10 @@
+// Cliccando su un pulsante, viene avviato un cronometro. Per fermare il cronometro, bisogna cliccare su un secondo pulsante
+
+const btnStart = document.querySelector("button#start");
+const btnStop = document.querySelector("button#stop");
+
+btnStart.addEventListener("click",  )
+//nello start
+
+btnStop.addEventListener("cick", )
+//nello stop devo mettere clearInterval
