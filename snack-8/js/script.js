cb79db81b1c8dd9df2b404e86a3571072347c6d5
federicaarosio <arosio.fede@gmail.com>
2023-11-24 12:31:25 +0100

    snack-8

---
+++ snack-8/js/script.js
@@ -0,0 +1,47 @@
+// Crea 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine.
+
+var zucchine = [
+    zucchina1 = {
+      varieta: 'fantastica',
+      peso: '1',
+      lunghezza: '20'
+    },
+    zucchina2 = {
+        varieta: 'blu',
+        peso: '1',
+        lunghezza: '20'
+      },
+      zucchina3 = {
+        varieta: 'americana',
+        peso: '1',
+        lunghezza: '20'
+      },
+      zucchina4 = {
+        varieta: 'contadina',
+        peso: '1',
+        lunghezza: '20'
+      },
+      zucchina5 = {
+        varieta: 'ogm',
+        peso: '1',
+        lunghezza: '20'
+      },
+      zucchina6 = {
+        varieta: 'verde',
+        peso: '1',
+        lunghezza: '20'
+      },
+      zucchina7 = {
+        varieta: 'gialla',
+        peso: '1',
+        lunghezza: '20'
+      },
+      zucchina8 = {
+        varieta: 'rigata',
+        peso: '1',
+        lunghezza: '20'
+      },
+  ];
+  console.log(zucchine);
+
+  const peso totale
